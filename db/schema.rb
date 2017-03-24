0fe1481414cf8c668090de0a5ad4fe988896f47f
MartinCrane <martin.crane@gmail.com>
2017-03-24 16:36:59 -0400

    db relationships established

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324181759) do
+ActiveRecord::Schema.define(version: 20170324203144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "genres", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = Movie.create([{ title: 'Star Wars', director: 'George Lucas', rating: '4'},
+                        { title: 'When Harry Met Sally', director: 'Rob Reiner', rating: '5'},
+                        { title: 'The Birds', director: 'Alfred Hitchcock', rating: '4' }])
+
+genres = Genre.create([{ name: 'Rom Com' },
+                        { name: 'Action' },
+                        { name: 'Sci Fi' },
+                        { name: 'Horror' }])
+
+movies = User.create([{ firstname: 'Sally', lastname: 'Smith' },
+                      { firstname: 'Timothy', lastname: 'Bruger'},
+                      { firstname: 'John', lastname: 'Henry' }
+                      ])
+
+accounts = Account.create([{ email: 'Sally@sally.com', password: 'password' },
+                      { email: 'Timothy@sally.com', password: 'password'},
+                      { email: 'John@sally.com', password: 'password' }
+                      ])
